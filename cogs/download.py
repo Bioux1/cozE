fb36b42b675906c6334635e564e9d2dc7ad7e793
Bioux <briac.joessel@gmail.com>
2023-06-09 20:36:06 +0200

    feat: add downloading progress message and disable console logs

--- cogs/download.py
+++ cogs/download.py
@@ -14,24 +14,26 @@ class Download(commands.Cog):
         self.client = client
         self.reddit = asyncpraw.Reddit(client_id = config["REDDIT_CLIENT"], client_secret = config["REDDIT_SECRET"], user_agent = config["REDDIT_USER"])
         self.downloader = Downloader(max_q = True)
+        self.downloader.log = False
 
     @commands.command(aliases = ["dl", "save", "fetch"])
     async def download(self, ctx, post = None):
         if post:
             try:
                 submission = await self.reddit.submission(url = post)
+                response = await ctx.send("Downloading...")
             except Exception as e:
                 await ctx.send(f"{ctx.author.mention}, Error getting the Reddit post: " + str(e))
                 return
             if submission.is_video:
                 self.downloader.url = submission.url
                 file = self.downloader.download()
-                await ctx.send(f"{ctx.author.mention}, here is the video:", file = discord.File(file))
+                await response.edit(content = f"{ctx.author.mention}, here is the video:", attachments = [discord.File(file)])
                 os.remove(file)
             elif submission.is_reddit_media_domain:
-                await ctx.send(f"{ctx.author.mention}, {submission.url}")
+                await response.edit(content = f"{ctx.author.mention}, {submission.url}")
             else:
-                await ctx.send(f"{ctx.author.mention}, post does not contain any media.")
+                await response.edit(content = f"{ctx.author.mention}, post does not contain any media.")
         else:
             await ctx.send(f"{ctx.author.mention}, you need to specify a post url.")
 
