4edf188388156f46e7b259b7bd7efb54507dc571
Bioux <55360592+Bioux1@users.noreply.github.com>
2023-06-26 15:02:55 +0200

    refactor: move events to a cog (#10)

---
+++ cogs/events.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+
+class Events(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"Logged as {self.client.user} (ID: {self.client.user.id})")
+
+    @commands.Cog.listener()
+    async def on_command_error(self, error):
+        if isinstance(error, commands.errors.CommandNotFound):
+            pass
+
+async def setup(client: commands.Bot):
+    await client.add_cog(Events(client))
